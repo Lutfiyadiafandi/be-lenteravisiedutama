c86f146096cc6e184c16d1f5034fec4b6119641b
Lutfiyadi Afandi <lutfiyadiafandi@gmail.com>
2023-08-04 16:26:40 +0800

    fix cross side issue

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: ['http://localhost:3000', 'https://lenteravisiedutama.com'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+
+  const sess: session.SessionOptions = {
+    secret: 'keyboard cat',
+    cookie: {},
+  };
+
+  if (app.get('env') === 'production') {
+    app.getHttpAdapter().getInstance().set('trust proxy', 1);
+    sess.cookie.secure = true;
+    sess.cookie.sameSite = 'none';
+    app.use(function (req, res, next) {
+      res.setHeader(
+        'Set-Cookie',
+        'cross-site-cookie=whatever; SameSite=None; Secure',
+      );
+      next();
+    });
+  }
+  app.use(session(sess));
   await app.listen(4000);
 }
 bootstrap();
